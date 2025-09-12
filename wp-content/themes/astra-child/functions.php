83bf33bb17bda6559b942f5b6fb2ccb95dca56b5
Son Le <sonsinger.sl@gmail.com>
2025-09-12 15:51:32 +0700

    [Theme] Fix error order by

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -481,7 +481,7 @@ add_action('elementor/frontend/widget/before_render', function($widget){
 
     $cb = function($args, $taxonomies) use ($order){
         if (!empty($taxonomies) && in_array('product_cat', (array)$taxonomies, true)) {
-            $args['orderby'] = 'order';
+            $args['orderby'] = 'meta_value_num';
             if ($order) $args['order'] = $order;
         }
 
